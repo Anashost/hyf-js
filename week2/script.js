a0304b83d83b72947343f9c0b8f09bbf0f0e4a7f
Anashost <Anasons69@gmail.com>
2018-09-14 01:37:04 +0200

    add week2

---
+++ week2/script.js
@@ -0,0 +1,111 @@
+//1
+console.log("Hello world");
+console.log("Halo");
+console.log("Hola");
+console.log("مرحيا");
+
+//2
+console.log('I\'m awesome');
+
+//3
+let x;
+console.log('the value of my variable x will be: undefined');
+x = 4 ;
+console.log(x);
+console.log('the value of x will be: 4');
+console.log(x);
+
+//4
+let y = "Hello java" ;
+console.log('the value of my string will be: Hello java ');
+console.log(y);
+let y = "10" ;
+console.log('the value of my string will be: 10');
+console.log(y);
+
+//5
+let z = 7.25 ;
+console.log(z);
+let a = Math.round(z);
+console.log(a);
+
+
+if(z > a) {
+    let j = z ; //j is the highest value
+}
+else{
+    z = a ;
+}
+
+console.log(j);
+
+//6
+let firstArray = [];
+console.log('the value of my Array will be: undefined ');
+console.log(firstArray);
+
+let animalsArray = ["cat" , "tiger" , "falcon" ];
+console.log(animalsArray);
+animalsArray.push("baby pig");
+console.log(animalsArray);
+
+//7
+let myString = "this is a test" ;
+console.log(myString);
+
+let myStrLength = myString.length ;
+console.log(myString);
+
+//8
+let type1 = "Hello" ;
+console.log(type1);
+
+let type2 = 7.50 ;
+console.log(type2);
+
+let type3 = false ;
+console.log(type3);
+
+let type4 = ["cat" , "dog" , "hamster"];
+console.log(type4);
+
+let type5 = true ;
+console.log(type5);
+
+if (type1 === type2) {
+    console.log("SAME TYPE");
+}
+
+else {
+    console.log("not SAME TYPE");
+}
+    
+
+if (type1 === type3) {
+    console.log("SAME TYPE");
+}
+
+else {
+    console.log("not SAME TYPE");
+}
+
+if (type1 === type4) {
+    console.log("SAME TYPE");
+}
+
+else {
+    console.log("not SAME TYPE");
+}
+
+if (type1 === type5) {
+    console.log("SAME TYPE");
+}
+
+else {
+    console.log("not SAME TYPE");
+}
+
+//9   // will complete tomorrow
+
+
+//10   // will complete tomorrow
